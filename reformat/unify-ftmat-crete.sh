847b761c1ef1ab50e7db04c948d03adcc3e8061d
jordan2lee <jordanl.7890@gmail.com>
2020-04-02 16:31:49 -0700

    concat gnosis feature files into 1

---
+++ reformat/unify-ftmat-crete.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ../data/original_pred_matrices/Gnosis_results/2020-03-10_extracted/
+
+echo 'will concat these files:'
+ls -1  | grep .*features_sets*
+
+
+{ cat ACC_features_sets.tsv; sed '1d' CESC_features_sets.tsv; \
+    sed '1d' ESCC_features_sets.tsv; \
+    sed '1d' KIRCKICH_features_sets.tsv; \
+    sed '1d' KIRP_features_sets.tsv; \
+    sed '1d' LIHCCHOL_features_sets.tsv; \
+    sed '1d' MESO_features_sets.tsv; \
+    sed '1d' PAAD_features_sets.tsv; \
+    sed '1d' PCPG_features_sets.tsv; \
+    sed '1d' PRAD_features_sets.tsv; \
+    sed '1d' SARC_features_sets.tsv; \
+    sed '1d' TGCT_features_sets.tsv; \
+    sed '1d' THYM_features_sets.tsv; \
+    sed '1d' UVM_features_sets.tsv; \
+    } > ../../../../reformat/concat--2020-03-10_extracted--features_sets.tsv
