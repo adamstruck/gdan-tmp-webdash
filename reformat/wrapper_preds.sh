99104d1f6ea1c6e6da99482340c47d5b08bfb2b4
jordan2lee <jordanl.7890@gmail.com>
2020-04-13 16:33:23 -0700

    incorp PR comments https://github.com/bmeg/gdan-tmp-webdash/pull/4

--- reformat/wrapper_preds.sh
+++ reformat/wrapper_preds.sh
@@ -1,60 +1,60 @@
 #!/bin/bash
 
 raw_dir=${1}
-step2_input=${2}
+path_to_temp=${2}
 output_dir=${3}
 
-mkdir temp_preds
+mkdir ${path_to_temp}/temp_preds
 
 # reformat prediction files
     # TODO : add loop to process all files
 python3 reformat-gnosis-preds.py \
     -in ${raw_dir}/ACC_single_sample_predictions.tsv \
-    -out ${step2_input}/reformat-ACC_single_sample_predictions.tsv
+    -out ${path_to_temp}/temp_preds/reformat-ACC_single_sample_predictions.tsv
 python3 reformat-gnosis-preds.py \
     -in ${raw_dir}/CESC_single_sample_predictions.tsv \
-    -out ${step2_input}/reformat-CESC_single_sample_predictions.tsv
+    -out ${path_to_temp}/temp_preds/reformat-CESC_single_sample_predictions.tsv
 python3 reformat-gnosis-preds.py \
     -in ${raw_dir}/ESCC_single_sample_predictions.tsv \
-    -out ${step2_input}/reformat-ESCC_single_sample_predictions.tsv
+    -out ${path_to_temp}/temp_preds/reformat-ESCC_single_sample_predictions.tsv
 python3 reformat-gnosis-preds.py \
     -in ${raw_dir}/KIRCKICH_single_sample_predictions.tsv \
-    -out ${step2_input}/reformat-KIRCKICH_single_sample_predictions.tsv
+    -out ${path_to_temp}/temp_preds/reformat-KIRCKICH_single_sample_predictions.tsv
 python3 reformat-gnosis-preds.py \
     -in ${raw_dir}/KIRP_single_sample_predictions.tsv \
-    -out ${step2_input}/reformat-KIRP_single_sample_predictions.tsv
+    -out ${path_to_temp}/temp_preds/reformat-KIRP_single_sample_predictions.tsv
 python3 reformat-gnosis-preds.py \
     -in ${raw_dir}/LIHCCHOL_single_sample_predictions.tsv \
-    -out ${step2_input}/reformat-LIHCCHOL_single_sample_predictions.tsv
+    -out ${path_to_temp}/temp_preds/reformat-LIHCCHOL_single_sample_predictions.tsv
 python3 reformat-gnosis-preds.py \
     -in ${raw_dir}/MESO_single_sample_predictions.tsv \
-    -out ${step2_input}/reformat-MESO_single_sample_predictions.tsv
+    -out ${path_to_temp}/temp_preds/reformat-MESO_single_sample_predictions.tsv
 python3 reformat-gnosis-preds.py \
     -in ${raw_dir}/PAAD_single_sample_predictions.tsv \
-    -out ${step2_input}/reformat-PAAD_single_sample_predictions.tsv
+    -out ${path_to_temp}/temp_preds/reformat-PAAD_single_sample_predictions.tsv
 python3 reformat-gnosis-preds.py \
     -in ${raw_dir}/PCPG_single_sample_predictions.tsv \
-    -out ${step2_input}/reformat-PCPG_single_sample_predictions.tsv
+    -out ${path_to_temp}/temp_preds/reformat-PCPG_single_sample_predictions.tsv
 python3 reformat-gnosis-preds.py \
     -in ${raw_dir}/PRAD_single_sample_predictions.tsv \
-    -out ${step2_input}/reformat-PRAD_single_sample_predictions.tsv
+    -out ${path_to_temp}/temp_preds/reformat-PRAD_single_sample_predictions.tsv
 python3 reformat-gnosis-preds.py \
     -in ${raw_dir}/SARC_single_sample_predictions.tsv \
-    -out ${step2_input}/reformat-SARC_single_sample_predictions.tsv
+    -out ${path_to_temp}/temp_preds/reformat-SARC_single_sample_predictions.tsv
 python3 reformat-gnosis-preds.py \
     -in ${raw_dir}/TGCT_single_sample_predictions.tsv \
-    -out ${step2_input}/reformat-TGCT_single_sample_predictions.tsv
+    -out ${path_to_temp}/temp_preds/reformat-TGCT_single_sample_predictions.tsv
 python3 reformat-gnosis-preds.py \
     -in ${raw_dir}/THYM_single_sample_predictions.tsv \
-    -out ${step2_input}/reformat-THYM_single_sample_predictions.tsv
+    -out ${path_to_temp}/temp_preds/reformat-THYM_single_sample_predictions.tsv
 python3 reformat-gnosis-preds.py \
     -in ${raw_dir}/UVM_single_sample_predictions.tsv \
-    -out ${step2_input}/reformat-UVM_single_sample_predictions.tsv
+    -out ${path_to_temp}/temp_preds/reformat-UVM_single_sample_predictions.tsv
 
 
 # Clean up code
 python3 reformat-gnosis-preds-STEP2.py \
-    -in ${step2_input} \
+    -in ${path_to_temp} \
     -out ${output_dir}
 
 rm -r temp_preds
